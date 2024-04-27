adf1c8ee0a3313dbf9f83516dcfb140284f3e389
ahtasham2001 <ahtashamse2001@gmail.com>
2024-04-28 04:34:43 +0500

    Array Initial Files

---
+++ ARRAYJS24/app.js
@@ -0,0 +1,25 @@
+// What is an Array?
+// An array in JavaScript is a special type of object that stores a collection of elements. These elements can be of any data type, including numbers, strings, objects, or even other arrays. Arrays in JavaScript are dynamic, meaning they can grow or shrink in size dynamically as elements are added or removed.
+
+// Creating Arrays
+// In JavaScript, we can create an array using
+
+// Array Literal Notation '[]'
+// Array constructor
+// Example:
+
+// Array Literal Notation
+// In JavaScript, we can create arrays using array literal notation, which involves enclosing a comma-separated list of elements within square brackets '[]'.
+
+// Creating an array of numbers using array literal notation
+const numbers = [1,2,3,4,5,6];
+console.log(numbers);
+console.log(typeof(numbers));
+// Creating an array of strings using array literal notation
+const fruits = ["apple", "banana", "orange"]
+console.log(fruits);
+console.log(typeof(fruits));
+// Creating an array of mixed data types using array literal notation
+const mixed = [1, 'apple',true,{ name: "Ahtasham"}];
+console.log(mixed);
+console.log(typeof(mixed));

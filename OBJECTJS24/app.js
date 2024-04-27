acb1737c54defe939bf12b7eb1748abdc725bac1
ahtasham2001 <ahtashamse2001@gmail.com>
2024-04-27 07:32:36 +0500

    Object Creat Empty

--- OBJECTJS24/app.js
+++ OBJECTJS24/app.js
@@ -54,6 +54,41 @@
 // console.log(person);
 // console.log(typeof(person));
 
+// Using Object.create() method
+// In JavaScript, Object.create() is another method to create objects. However, it involves an additional step compared to some other object creation methods. First, a prototype object is created beforehand. Then, new objects are created using that prototype. Afterward, properties can be added or modified on the newly created object.
+
+// Let's dive into an example of using Object.create() to create objects in JavaScript.
+
+
+
+// let personPrototype = {
+//     name : "Ahtasham"
+// };
+
+// let person = Object.create(personPrototype);
+// console.log(person);
+
+
+// An important thing to note is, while we are using Object.create(), this method creates a new empty object. In the above example person is a new empty object.
+
+// In the above example, person is created with personPrototype as its prototype, but no additional properties are added to it, resulting in an empty object.
+
+// Now, we can dynamically add properties to the person object.
+
+// Example : Let's add firstName, lastName, age and hobbies dynamically to the newly created person object.
+// let personPrototype = {
+//     name : "Ahtasham"
+// };
+
+// let person = Object.create(personPrototype);
+// console.log(person);
+
+// person.firstName = "Ahtasham";
+// person.lastName = "Naeem";
+// person.age = 22;
+// person.hobbies = ["HardWork","Learn New Skill", "Reading"]
+
+// console.log(person);
 
 
 

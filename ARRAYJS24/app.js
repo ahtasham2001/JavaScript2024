132223ff8a993e077332636fd833337d9d7e9dab
ahtasham2001 <ahtashamse2001@gmail.com>
2024-04-28 04:43:25 +0500

    Array Construtor

--- ARRAYJS24/app.js
+++ ARRAYJS24/app.js
@@ -12,14 +12,33 @@
 // In JavaScript, we can create arrays using array literal notation, which involves enclosing a comma-separated list of elements within square brackets '[]'.
 
 // Creating an array of numbers using array literal notation
-const numbers = [1,2,3,4,5,6];
-console.log(numbers);
-console.log(typeof(numbers));
-// Creating an array of strings using array literal notation
-const fruits = ["apple", "banana", "orange"]
-console.log(fruits);
-console.log(typeof(fruits));
-// Creating an array of mixed data types using array literal notation
-const mixed = [1, 'apple',true,{ name: "Ahtasham"}];
-console.log(mixed);
-console.log(typeof(mixed));
+// const numbers = [1,2,3,4,5,6];
+// console.log(numbers);
+// console.log(typeof(numbers));
+// // Creating an array of strings using array literal notation
+// const fruits = ["apple", "banana", "orange"]
+// console.log(fruits);
+// console.log(typeof(fruits));
+// // Creating an array of mixed data types using array literal notation
+// const mixed = [1, 'apple',true,{ name: "Ahtasham"}];
+// console.log(mixed);
+// console.log(typeof(mixed));
+
+
+// Array Constructor
+// In JavaScript, we can also create arrays using the Array constructor. The Array constructor can be called with or without the new keyword.
+// Creating an array of numbers using Array constructor
+
+// const numbers = new Array(1,2,3,4,5,6);
+// console.log(numbers);
+// console.log(typeof(numbers));
+
+// // Creating an array of strings using Array constructor
+
+// const fruits = new Array("apple","banana","orange");
+// console.log(fruits);
+// console.log(typeof(fruits));
+
+// // Creating an empty array using Array constructor
+// const emptyArray = new Array();
+// console.log(emptyArray);

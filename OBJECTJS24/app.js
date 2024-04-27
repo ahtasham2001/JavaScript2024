155ce61636fde12865caf31f13fb7aa40e7b6b79
ahtasham2001 <ahtashamse2001@gmail.com>
2024-04-27 05:01:11 +0500

    initital files Object

---
+++ OBJECTJS24/app.js
@@ -0,0 +1,16 @@
+// bjects: JavaScript objects are fundamental data structures that enable the organization and storage of data in the form of key-value pairs. Objects are unordered collection of key value pairs. These key-value pairs, known as properties.
+
+// Object Creation in JavaScript
+// There can be typically four ways to create an Object in JavaScript.
+
+// Using object literal.
+// Using new keyword
+// Using Object.create() method
+// Using Factory functions
+// Using Constructor functions
+// Take a deep breath. Let's start with the first method.
+
+// Using object literal
+// This is the most common, easiest and straightforward way to create an object. We can define the object and its properties using the object literals.
+
+// Object literal is a list of key-value pairs enclosed in curly braces '{}'

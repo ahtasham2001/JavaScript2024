7735fabadd167aead10a48a1a504ccdc099c4e46
ahtasham2001 <ahtashamse2001@gmail.com>
2024-04-29 05:47:08 +0500

    funtion object

--- HOSTING&FUNCALL/app.js
+++ HOSTING&FUNCALL/app.js
@@ -27,3 +27,34 @@
 // // var fullName = "Ahtasham Naeem";
 // // let fullName = "Ahtasham Naeem";
 // const fullName = "Ahtasham Naeem";
+
+
+// function solve(number) {
+//     return function(number) {
+//         return number*number;
+//     }
+// }
+
+// let ans = solve(5);
+// let finalAns = ans(10);
+// console.log(finalAns);
+
+
+
+const arr = {
+    function(a,b){
+        return a+b;
+    },
+
+    function(a,b){
+        return a-b;
+    },
+
+    function(a,b){
+        return a*b;
+    }
+}
+
+let first = arr[0];
+let ans =first(10,4);
+console.log(ans);

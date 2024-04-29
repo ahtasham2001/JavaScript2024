2dc0490ad5f5fe24f4313a276bcdee75522261d2
ahtasham2001 <ahtashamse2001@gmail.com>
2024-04-29 06:23:39 +0500

    Temporal Dead Zone

--- VARSCOPING&DEADZONE24/app.js
+++ VARSCOPING&DEADZONE24/app.js
@@ -6,6 +6,31 @@
 // Local (Function) Scope
 // Block-Level Scope
 
-function addNumbers(){
-    var sum = 5+5;
-}
+// function addNumbers(){
+//     var sum = 5+5;
+// }
+
+
+
+// global scope
+// var age = 15;
+// console.log(age);
+// {
+//     console.log(age);
+// }
+// if(true){
+//     console.log(age);
+// }
+// function sayHello(){
+//     console.log("hi ", age);
+// }
+// sayHello();
+
+
+// Temporal Dead Zone
+
+// console.log(marks);
+// console.log("Ahtahsam");
+// console.log("Naeem");
+// const marks = 100;
+// console.log(marks);

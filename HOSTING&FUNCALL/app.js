efd6990126e3a90761b028b90593163d3a785e11
ahtasham2001 <ahtashamse2001@gmail.com>
2024-04-29 05:33:05 +0500

    Hositing and Function Call back

---
+++ HOSTING&FUNCALL/app.js
@@ -0,0 +1,22 @@
+
+
+// sayHello();
+// function sayHello(){
+//     console.log("Ahtasham");
+// }
+
+
+sayHello();
+
+// let sayHello = function(){
+//     console.log("Ahtasham");
+// }
+
+
+// var sayHello = function(){
+//     console.log("Ahtasham");
+// }
+
+// let sayHello = function(){
+//     console.log("Ahtasham");
+// }

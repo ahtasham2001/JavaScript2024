c70d8730fb48a41fb123a44302d80d28b565447e
ahtasham2001 <ahtashamse2001@gmail.com>
2024-04-29 06:10:19 +0500

    var scoping & Dead Zone

---
+++ VARSCOPING&DEADZONE24/app.js
@@ -0,0 +1,11 @@
+// In JavaScript, the scope of a variable determines where it can be accessed within the code.
+
+// Variables can be declared in different scopes:
+
+// Global Scope
+// Local (Function) Scope
+// Block-Level Scope
+
+function addNumbers(){
+    var sum = 5+5;
+}

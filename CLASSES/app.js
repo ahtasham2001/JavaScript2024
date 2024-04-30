fc5cf888f784eea1d988f8c90dc1d5d22592e8c7
ahtasham2001 <ahtashamse2001@gmail.com>
2024-04-30 05:53:19 +0500

    public vs Private

--- CLASSES/app.js
+++ CLASSES/app.js
@@ -1,13 +1,13 @@
 class user {
     // properties
-    age = 22;
-    weight = 55;
+    age = 22; //public
+    #weight = 55; //private
     ht = 180;
 
     // behaviour
 
     walking(){
-        console("I am Walking")
+        console("I am Walking", this.#weight)
     }
 
     Runing(){
@@ -17,3 +17,6 @@ class user {
 
 let obj = new user();
 console.log(obj.age);
+console.log(obj.weight);
+console.log(obj.ht);
+obj.walking

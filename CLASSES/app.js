4925bdcfb20c03058983ec9ff439af97bca38961
ahtasham2001 <ahtashamse2001@gmail.com>
2024-04-30 05:44:09 +0500

    Classes

---
+++ CLASSES/app.js
@@ -0,0 +1,19 @@
+class user {
+    // properties
+    age = 22;
+    weight = 55;
+    ht = 180;
+
+    // behaviour
+
+    walking(){
+        console("I am Walking")
+    }
+
+    Runing(){
+        console("I am Runing")
+    }
+}
+
+let obj = new user();
+console.log(obj.age);

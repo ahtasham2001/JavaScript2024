c88a2e0c96a023d6cef53d2dea8a2cf3adc3d407
ahtasham2001 <ahtashamse2001@gmail.com>
2024-05-01 05:47:37 +0500

    getter vs setter

--- GETTER&SETTER24/app.js
+++ GETTER&SETTER24/app.js
@@ -5,8 +5,23 @@ class User {
     
     }
 
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value.toUpperCase()
+    }
+
+    get password(){
+        return this._password.toUpperCase()
+    }
+    set password(value){
+       this._password = value.toUpperCase()
+    }
+
 }
 
 
 const ahtasham = new User("a@gmail.com", "124")
+console.log(ahtasham.password);
 console.log(ahtasham.email);

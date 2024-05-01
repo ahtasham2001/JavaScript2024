1f47300ae5002d9467fdf915ba9c28a4c15e2988
ahtasham2001 <ahtashamse2001@gmail.com>
2024-05-01 05:37:17 +0500

    Getter Setter Initial files

---
+++ GETTER&SETTER24/app.js
@@ -0,0 +1,12 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password;
+    
+    }
+
+}
+
+
+const ahtasham = new User("a@gmail.com", "124")
+console.log(ahtasham.email);
